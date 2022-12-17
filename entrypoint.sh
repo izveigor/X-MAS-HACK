68a73e4fd2efbeeaf81d537450c26e57de335b12
izveigor <izveigor@mail.ru>
2022-12-17 06:03:52 +0300

    Сделан первоначальный DevOps

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+docker-compose -f backend/account/docker-compose.yml up --build
+docker-compose -f backend/api/docker-compose.yml up --build
+docker-compose -f backend/gateway/docker-compose.yml up --build
+docker-compose -f backend/ml/docker-compose.yml up --build
+docker-compose -f Frontend/docker-compose.yml up --build
