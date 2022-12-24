f6cbe9a91986ff46aafab54ccebec4dbafd69b23
izveigor <izveigor@mail.ru>
2022-12-24 16:42:39 +0300

    ML refactoring

--- backend/ml/broker/consumer.py
+++ backend/ml/broker/consumer.py
@@ -1,54 +1,56 @@
-import pika
-from features.constants import BROKER_SETTINGS
 import threading
+from typing import Any
+
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+
+from features.constants import BROKER_SETTINGS, ID_SIZE
 from features.singleton import Singleton
-from features.constants import ID_SIZE
-from model import Model
+from model.model import DocumentModel
 
 
 class Consumer(metaclass=Singleton):
-    '''Получаем сообщение из RabbitMQ
+    """Получаем сообщение из RabbitMQ
 
     Атрибуты:
         _connection - соединение
         _channel - канал соединения
-    '''
+    """
 
-    _connection: None
-    _channel: None
+    _connection: pika.BlockingConnection
+    _channel: BlockingChannel
 
-    def __init__(self):
-        '''Инициализуруем работы очереди'''
+    def __init__(self) -> None:
+        """Инициализуруем работы очереди"""
         credentials = pika.PlainCredentials(
-            BROKER_SETTINGS['RABBITMQ_USERNAME'],
-            BROKER_SETTINGS['RABBITMQ_PASSWORD'],
+            BROKER_SETTINGS["RABBITMQ_USERNAME"],
+            BROKER_SETTINGS["RABBITMQ_PASSWORD"],
         )
 
         self._connection = pika.BlockingConnection(
             pika.ConnectionParameters(
-                host=BROKER_SETTINGS['RABBITMQ_HOST'],
-                port=BROKER_SETTINGS['RABBITMQ_PORT'],
+                host=BROKER_SETTINGS["RABBITMQ_HOST"],
+                port=BROKER_SETTINGS["RABBITMQ_PORT"],
                 credentials=credentials,
             )
         )
 
         self._channel = self._connection.channel()
-        self._channel.exchange_declare('document', durable=True, exchange_type='topic')
+        self._channel.exchange_declare("document", durable=False, exchange_type="direct")
         self._channel.basic_consume(
-            queue='API',
+            queue="API",
             on_message_callback=self._callback,
             auto_ack=True,
         )
 
         self.start()
 
-    def start(self):
-        '''Начинаем поток для чтения сообщений из очереди
-        '''
+    def start(self) -> None:
+        """Начинаем поток для чтения сообщений из очереди"""
         self._thread = threading.Thread(target=self._channel.start_consuming)
         self._thread.start()
 
-    def _callback(self, channel, method, properties, body):
-        '''Читаем наше сообщение, а затем предсказываем, спам ли это или нет'''
-        id_, file = body[:ID_SIZE].decode("utf-8"), body[ID_SIZE:].decode("utf-8")
-        Model().predict(id_, file)
+    def _callback(self, channel: Any, method: Any, properties: Any, body: bytes) -> None:
+        """Читаем наше сообщение, а затем предсказываем, спам ли это или нет"""
+        id_, file = body[:ID_SIZE].decode("utf-8"), body[ID_SIZE:]
+        DocumentModel().predict(id_, file)
