7cd554ea782b17870394f470dfc9785edb21a70b
izveigor <izveigor@mail.ru>
2022-12-26 11:12:33 +0300

    Reinitialize Java project

--- backend/account/restfulauth/entity/Status.java
+++
@@ -1,5 +0,0 @@
-package com.example.restfulauth.entity;
-
-public enum Status {
-    ACTIVE, BANNED
-}
--- backend/account/restfulauth/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.restfulauth.service;
-
-import com.example.restfulauth.entity.User;
-
-public interface UserService{
-    void save(User user);
-}
