1ce6109a63c3a091034834a4eba97ec21c81e197
izveigor <izveigor@mail.ru>
2022-12-26 20:02:38 +0300

    DevOps for account

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
+#!bin/sh
+
 docker-compose -f backend/account/docker-compose.yml up --build
 docker-compose -f backend/api/docker-compose.yml up --build
 docker-compose -f backend/gateway/docker-compose.yml up --build
 docker-compose -f backend/ml/docker-compose.yml up --build
 docker-compose -f Frontend/docker-compose.yml up --build
+
+exec "$@"
